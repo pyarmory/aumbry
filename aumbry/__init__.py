3e5fe75c272ada1020ce044697c91a20c2bb7426
John Vrbanac <john.vrbanac@linux.com>
2017-04-09 15:12:31 -0500

    Adding Attr passthrough

--- aumbry/__init__.py
+++ aumbry/__init__.py
@@ -8,11 +8,14 @@ from .formats.js import JsonConfig # NOQA
 from .formats.yml import YamlConfig # NOQA
 from .sources import SourceTypes
 
+from alchemize.mapping import Attr # NOQA
+
 FILE = SourceTypes.file
 CONSUL = SourceTypes.consul
 
 
 __all__ = [
+    'Attr',
     'load',
     'JsonConfig',
     'YamlConfig',
